ad11cf4b7d3c47225cff06e09e5a792b0b925b59
Jason Juhyun Kim <mtinet@hanmail.net>
2022-12-31 15:44:34 +0900

    Update firebase.js

--- web/public/js/firebase.js
+++ web/public/js/firebase.js
@@ -1,9 +1,12 @@
+// 변수 선언
 let humi = 0;
 let temp = 0;
 let light = 0;
 let fanStatus = Boolean(false);
 let ledStatus = Boolean(false);
 
+// firebase를 javascript로 사용하기 위한 config  
+// firebase 콘솔-프로젝트 개요-기어표시 클릭-프로젝트 설정-일반-내앱-앱 추가-firebaseConfig 안의 내용만 복사해서 붙여넣음  
 var config = {
   apiKey: "AIzaSyA4JNrHtS9pc6QaW8dtwATWhUhs0Ni8OBI",
   authDomain: "smartfarm-f867f.firebaseapp.com",
