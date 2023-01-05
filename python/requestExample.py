2e8489953dfda0a3f29fe7507be1f711fa38a16e
Jason Juhyun Kim <mtinet@hanmail.net>
2023-01-05 14:06:15 +0900

    Update requestExample.py

--- python/requestExample.py
+++ python/requestExample.py
@@ -12,6 +12,6 @@ response = requests.get(url+".json", verify=False).json()
 print(response)
 
 # 객체 교체하기, patch는 특정 주소의 데이터가 변경됨
-myobj = {'ㅋㅋ': 'ㄴㄴ', 'temp': random.randrange(0, 50), 'humi': random.randrange(0,100)}
+myobj = {'ㅋㅋ': 'ㄴㄴ', 'light': random.randrange(0,100), 'temp': random.randrange(0, 50), 'humi': random.randrange(0,100)}
 requests.patch(url+"smartFarm.json", json = myobj, verify=False).json()
 
