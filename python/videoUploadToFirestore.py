25a5040ac72902ed33a150dfa6756fc2412f9eac
Jason Juhyun Kim <mtinet@hanmail.net>
2023-01-05 15:54:14 +0900

    Update videoUploadToFirestore.py

--- python/videoUploadToFirestore.py
+++ python/videoUploadToFirestore.py
@@ -10,14 +10,14 @@ firebase_admin.initialize_app(cred, {
 })
 
 # 업로드할 이미지 파일을 열기 위해 파일 객체를 생성합니다.
-with open("video.mp4", "rb") as image_file:
+with open("video.mp4", "rb") as video_file:
     # 이미지 파일 데이터를 읽어옵니다.
-    image_data = image_file.read()
+    video_data = video_file.read()
 
     # 이미지 데이터를 이용해 Blob 객체를 생성합니다.
     bucket = storage.bucket()
-    blob = bucket.blob("video/viddddeo.mp4")
-    blob.upload_from_string(image_data, content_type='video/mp4')
+    blob = bucket.blob("video/video.mp4") # 올라갈 폴더와 파일명
+    blob.upload_from_string(video_data, content_type='video/mp4') # 콘텐츠 타입 설정
 
 # 업로드된 이미지의 URL을 가져옵니다.
 url = blob.public_url
