03253a9daffe38e62967a99a09d36bc1da0d06d5
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-02 23:23:06 +0900

    Add files via upload

---
+++ 0. pico_test/6. OLED/randomName.py
@@ -0,0 +1,21 @@
+from machine import Pin, I2C
+from ssd1306 import SSD1306_I2C
+import random
+import time
+
+i2c=I2C(0,sda=Pin(0), scl=Pin(1), freq=100000)
+print("I2C : " + str(i2c))
+oled = SSD1306_I2C(128, 64, i2c)
+
+while True:
+    oled.fill(0)
+    cmd = ["hihi", "junse", "nini", "babo"]
+    ran = random.randint(0,len(cmd)-1)
+    print(ran)
+
+    oled.text(cmd[ran], 10, 10)
+    
+    oled.show()
+    time.sleep(1)
+
+
