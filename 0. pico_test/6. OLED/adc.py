b1ea809f078b8df639621d7942036a3ce2394a56
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-06 00:45:28 +0900

    Update adc.py

--- 0. pico_test/6. OLED/adc.py
+++ 0. pico_test/6. OLED/adc.py
@@ -11,7 +11,7 @@ conversion_factor = 3.3 / (65535)
 WIDTH  = 128                                            # oled display width
 HEIGHT = 64                                             # oled display height
  
-i2c = I2C(0, scl=Pin(1), sda=Pin(0), freq=200000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
+i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=200000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
 print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
 print("I2C Configuration: "+str(i2c))                   # Display I2C config
 
