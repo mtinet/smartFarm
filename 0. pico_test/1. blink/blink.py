86c2127119716442727eae97184302ded5c9ccfd
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-06 00:17:02 +0900

    Update blink.py

--- 0. pico_test/1. blink/blink.py
+++ 0. pico_test/1. blink/blink.py
@@ -1,10 +1,13 @@
 from machine import Pin
 import time
 
+# 빌트인 LED 모드 설정
 led = Pin("LED", Pin.OUT)
 
+# 반복동작
 while True:
-    led.off()
-    time.sleep(1)
-    led.on()
-    time.sleep(1)
+   led.off()
+   time.sleep(1)
+   led.on()
+   time.sleep(1)
+
