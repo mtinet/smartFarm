a2ca9640cbb2e9ada333e4ec66dbcfafb5b8aa45
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-11 22:19:38 +0900

    Update DHT11.py

--- 0. pico_test/10. DHT11/DHT11.py
+++ 0. pico_test/10. DHT11/DHT11.py
@@ -3,9 +3,9 @@ import utime as time
 from dht import DHT11, InvalidChecksum
 
 while True:
-    time.sleep(1.5)
     pin = Pin(15, Pin.OUT, Pin.PULL_DOWN)
     sensor = DHT11(pin)
-    t  = (sensor.temperature)
-    h = (sensor.humidity)
+    t  = sensor.temperature
+    h = sensor.humidity
     print("Temperature: {}".format(t), "Humidity: {}".format(h))
+    time.sleep(1.5)
