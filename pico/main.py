5f48897f320aa4f4abf500b147c97404b192d8a3
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-12 13:29:31 +0900

    Update main.py

--- pico/main.py
+++ pico/main.py
@@ -3,8 +3,8 @@ import network
 import time
 import urequests
 import random
-from machine import Pin
-
+from ssd1306 import SSD1306_I2C
+import framebuf
 
 # 제어할 핀 번호 설정
 led = Pin(1, Pin.OUT) # 생장 LED제어 핀
@@ -13,7 +13,35 @@ moisture = ADC(26) # 수분 감지
 temperature = ADC(27) # 온도 감지
 light = ADC(28) # 조도 감지
 
-conversion_factor = 3.3 / 65535
+conversion_factor = 3.3 / 65535 # 측정값 보정 계산식 
+
+
+# OLED 기본 설정
+WIDTH  = 128                                            # oled display width
+HEIGHT = 64                                             # oled display height
+ 
+i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=200000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
+print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
+print("I2C Configuration: "+str(i2c))                   # Display I2C config
+
+oled = SSD1306_I2C(WIDTH, HEIGHT, i2c)                  # Init oled display
+
+# Raspberry Pi logo as 32x32 bytearray
+buffer = bytearray(b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00|?\x00\x01\x86@\x80\x01\x01\x80\x80\x01\x11\x88\x80\x01\x05\xa0\x80\x00\x83\xc1\x00\x00C\xe3\x00\x00~\xfc\x00\x00L'\x00\x00\x9c\x11\x00\x00\xbf\xfd\x00\x00\xe1\x87\x00\x01\xc1\x83\x80\x02A\x82@\x02A\x82@\x02\xc1\xc2@\x02\xf6>\xc0\x01\xfc=\x80\x01\x18\x18\x80\x01\x88\x10\x80\x00\x8c!\x00\x00\x87\xf1\x00\x00\x7f\xf6\x00\x008\x1c\x00\x00\x0c \x00\x00\x03\xc0\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00")
+
+# OLED에 출력하기
+oled.fill(0)
+# 프레임버퍼로 로고 불러오기(이미지 사이즈는  32x32)
+fb = framebuf.FrameBuffer(buffer, 32, 32, framebuf.MONO_HLSB)
+# 프레임 버퍼에서 OLED 디스플레이로 이미지 옮기기 
+oled.blit(fb, 96, 0)
+# 글자 넣기
+oled.text("SmartFarm", 0, 25)
+oled.text("  has been", 0, 35)
+oled.text("    initialized.", 0, 45)
+# 이미지와 글자가 보여지도록 하기 
+oled.show()
+    
 
 # 이메일, 위도, 경도 표시하기(자신의 스마트팜 위치를 검색해서 넣어주세요.)
 email = 'mtinet@hanmail.net'
@@ -61,12 +89,13 @@ myLocation = {
     }
 # myLocation를 RTDB로 보내 객체 교체하기, patch는 특정 주소의 데이터가 변경됨
 urequests.patch(url+"location.json", json = myLocation).json()
-# myLocation를 위치 수집용 RTDB로 보내기
+# myLocation를 스마트팜 위치 수집용 RTDB로 보내기
 urequests.patch(mapUrl+"location.json", json = myLocation).json()
 print("Location Info has been sent.")
 print()
-    
-# DB 내역 가져오기
+
+
+# RTDB 내역 가져오기
 response = urequests.get(url+".json").json()
 # byte형태의 데이터를 json으로 변경했기 때문에 메모리를 닫아주는 일을 하지 않아도 됨
 # print(response)
@@ -81,33 +110,51 @@ while True:
     temperatureValue = round((temperature.read_u16() * conversion_factor) * 100) # 온도센서 값 읽어오기
     lightValue = round((light.read_u16()/65535) * 100) # 조도센서 값 읽어오기
     
+    # 읽어온 RTDB값과 센서 값 콘솔에 출력하기
     print("Status Check")
     print("LED:", response['smartFarm']['led'], "Fan:", response['smartFarm']['fan'], "Moisture:", moistureValue, "Temperature:", temperatureValue, "Light:", lightValue )
     print()
     
-    # 현재 DB의 led 키 값의 상태에 따라 led 26번을 제어
+    # OLED에 출력하기
+    oled.fill(0)
+    # 프레임버퍼로 로고 불러오기(이미지 사이즈는  32x32)
+    fb = framebuf.FrameBuffer(buffer, 32, 32, framebuf.MONO_HLSB)
+    # 프레임 버퍼에서 OLED 디스플레이로 이미지 옮기기 
+    oled.blit(fb, 96, 0)
+    # 글자 넣기
+    oled.text("Light: ", 0, 10)
+    oled.text(str(round(lightValue,2)), 75, 10)
+    oled.text("Temp: ", 0, 25)
+    oled.text(str(round(temperatureValue,2)), 75, 25)
+    oled.text("Moisture: ", 0, 40)
+    oled.text(str(round(moistureValue,2)), 75, 40)
+    # 이미지와 글자가 보여지도록 하기 
+    oled.show()
+    
+    # 현재 RTDB의 led 키 값의 상태에 따라 LED 핀(1번)을 제어
     if (response['smartFarm']['led'] == 0) :
         led.value(0)
     else :
         led.value(1)
     
-    # 현재 DB의 fan 키 값의 상태에 따라 led 27번을 제어
+    # 현재 RTDB의 fan 키 값의 상태에 따라 Fan 핀(5번)을 제어
     if (response['smartFarm']['fan'] == 0) :
         fan.value(0)
     else :
         fan.value(1)
 
-    # 각 객체 값 교체하기
+    # 실시간으로 확인된 각 객체 값을 딕셔너리에 넣기
     myobj = {
         'mois': moistureValue,
         'temp': temperatureValue,
         'light': lightValue        
         }
     
-    # myobj를 RTDB로 보내 객체 교체하기, patch는 특정 주소의 데이터가 변경됨
+    # myobj를 RTDB로 보내 객체 값 교체하기, patch는 특정 주소의 데이터가 변경됨
     urequests.patch(url+"smartFarm.json", json = myobj).json()
     
-    # 교체한 객체값 모니터링하기 
+    # 교체한 객체값 콘솔에 출력하기 
     print("Message Send")
     print(myobj)
     print()
+
