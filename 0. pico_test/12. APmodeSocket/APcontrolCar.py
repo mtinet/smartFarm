e56512619c43ae6008063d5d06d858c290f29032
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-26 23:56:07 +0900

    Create APcontrolCar.py

---
+++ 0. pico_test/12. APmodeSocket/APcontrolCar.py
@@ -0,0 +1,36 @@
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <title>Zumo Robot Control</title>
+            <center><H1>IoT Car</h1></center>
+        </head>
+        <body>
+            <center>
+                <form action="./forward">
+                    <input type="submit" value="Forward" style="height:300px; width:300px; font-size:30px" />
+                </form>
+                <table>
+                    <tr>
+                        <td>
+                            <form action="./left">
+                                <input type="submit" value="Left" style="height:300px; width:300px; font-size:30px" />
+                            </form>
+                        </td>
+                        <td>
+                            <form action="./stop">
+                                <input type="submit" value="Stop" style="height:300px; width:300px; font-size:30px" />
+                            </form>
+                        </td>
+                        <td>
+                            <form action="./right">
+                                <input type="submit" value="Right" style="height:300px; width:300px; font-size:30px" />
+                            </form>
+                        </td>
+                    </tr>
+                </table>
+                <form action="./back">
+                    <input type="submit" value="Back" style="height:300px; width:300px; font-size:30px" />
+                </form>
+            </center>
+        </body>
+    </html>
