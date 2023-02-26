b43b40f5928c04a3fdb7498775e87631c59a51ea
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-26 20:55:13 +0900

    Create timeRequest.py

---
+++ 0. pico_test/11. timeRequest/timeRequest.py
@@ -0,0 +1,21 @@
+import network
+import time
+import urequests
+
+# 와이파이 연결하기
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+if not wlan.isconnected():
+    wlan.connect("U+Net454C", "DDAE014478")
+    print("Waiting for Wi-Fi connection", end="...")
+    while not wlan.isconnected():
+        print(".", end="")
+        time.sleep(1)
+else:
+    print(wlan.ifconfig())
+    print("WiFi is Connected")
+    
+
+# 시간정보 가져오기
+from timezoneChange import timeOfSeoul
+timeOfSeoul()
