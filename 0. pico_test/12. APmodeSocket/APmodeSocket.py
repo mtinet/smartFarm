59a6dd8bdebf007db085261382f8ea36e1315aad
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-26 23:15:24 +0900

    Add files via upload

---
+++ 0. pico_test/12. APmodeSocket/APmodeSocket.py
@@ -0,0 +1,48 @@
+import socket
+import network
+import machine
+
+ssid = 'MicroPython-AP'
+password = '123456789'
+
+led = machine.Pin("LED",machine.Pin.OUT)
+
+ap = network.WLAN(network.AP_IF)
+ap.config(essid=ssid, password=password)
+ap.active(True)
+
+while ap.active() == False:
+  pass
+
+print('Connection successful')
+print(ap.ifconfig())
+
+html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+  <body><h1>Welcome to microcontrollerslab!</h1></body></html>
+"""
+
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+led.off()
+
+# Listen for connections
+while True:
+    try:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024)
+        led.on()
+        print(request)
+
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(html)
+        cl.close()
+        led.off()
+
+    except OSError as e:
+        cl.close()
+        print('connection closed')
