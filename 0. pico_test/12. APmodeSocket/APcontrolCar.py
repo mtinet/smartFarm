1787d838eec4fd92ac6b29a7b80edf0b4b5ddb44
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-27 00:02:39 +0900

    Update APcontrolCar.py

--- 0. pico_test/12. APmodeSocket/APcontrolCar.py
+++ 0. pico_test/12. APmodeSocket/APcontrolCar.py
@@ -30,29 +30,29 @@ html = f"""
         <body>
             <center>
                 <form action="./forward">
-                    <input type="submit" value="Forward" style="height:300px; width:300px; font-size:30px" />
+                    <input type="submit" value="Forward" style="height:100px; width:100px; font-size:10px" />
                 </form>
                 <table>
                     <tr>
                         <td>
                             <form action="./left">
-                                <input type="submit" value="Left" style="height:300px; width:300px; font-size:30px" />
+                                <input type="submit" value="Left" style="height:100px; width:100px; font-size:10px" />
                             </form>
                         </td>
                         <td>
                             <form action="./stop">
-                                <input type="submit" value="Stop" style="height:300px; width:300px; font-size:30px" />
+                                <input type="submit" value="Stop" style="height:100px; width:100px; font-size:10px" />
                             </form>
                         </td>
                         <td>
                             <form action="./right">
-                                <input type="submit" value="Right" style="height:300px; width:300px; font-size:30px" />
+                                <input type="submit" value="Right" style="height:100px; width:100px; font-size:10px" />
                             </form>
                         </td>
                     </tr>
                 </table>
                 <form action="./back">
-                    <input type="submit" value="Back" style="height:300px; width:300px; font-size:30px" />
+                    <input type="submit" value="Back" style="height:100px; width:100px; font-size:10px" />
                 </form>
             </center>
         </body>
