002af39704a314ddc380203a4801efcf5563f817
Jason Juhyun Kim <mtinet@hanmail.net>
2023-02-26 23:59:25 +0900

    Update APcontrolCar.py

--- 0. pico_test/12. APmodeSocket/APcontrolCar.py
+++ 0. pico_test/12. APmodeSocket/APcontrolCar.py
@@ -1,6 +1,29 @@
+import socket
+import network
+import machine
+
+ssid = 'mtinet'
+password = '123456789'
+
+led = machine.Pin("LED",machine.Pin.OUT)
+
+ap = network.WLAN(network.AP_IF)
+ap.config(essid=ssid, password=password)
+ap.active(True)
+
+while ap.active() == False:
+  pass
+
+print('Connection successful')
+print(ap.ifconfig())
+
+#Template HTML
+html = f"""
     <!DOCTYPE html>
     <html>
         <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1">
             <title>Zumo Robot Control</title>
             <center><H1>IoT Car</h1></center>
         </head>
@@ -34,3 +57,31 @@
             </center>
         </body>
     </html>
+"""
+
+addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+s = socket.socket()
+s.bind(addr)
+s.listen(1)
+
+print('listening on', addr)
+led.off()
+
+# Listen for connections
+while True:
+    try:
+        cl, addr = s.accept()
+        print('client connected from', addr)
+        request = cl.recv(1024)
+        led.on()
+        print(request)
+
+        cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
+        cl.send(html)
+        cl.close()
+        led.off()
+
+    except OSError as e:
+        cl.close()
+        print('connection closed')
+
