2750d3174ca3cc5dbeb573b10be6998b806920a3
Jason Juhyun Kim <mtinet@hanmail.net>
2023-03-10 00:45:25 +0900

    Add files via upload

---
+++ 0. pico_test/13. AHT10/aht10.py
@@ -0,0 +1,16 @@
+import utime
+from machine import Pin, I2C
+
+import ahtx0
+
+# I2C for the Wemos D1 Mini with ESP8266
+i2c = I2C(1, scl=Pin(15), sda=Pin(14), freq=200000)
+
+# Create the sensor object using I2C
+sensor = ahtx0.AHT10(i2c)
+
+while True:
+    print("\nTemperature: %0.2f C" % ((sensor.temperature)-5))
+    print("Humidity: %0.2f %%" % sensor.relative_humidity)
+    utime.sleep(1)
+
