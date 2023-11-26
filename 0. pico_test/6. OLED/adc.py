fee2cbde72f763a15da44baf064f41c605f36114
Jason Juhyun Kim <mtinet@hanmail.net>
2023-11-27 00:11:46 +0900

    Update adc.py

--- 0. pico_test/6. OLED/adc.py
+++ 0. pico_test/6. OLED/adc.py
@@ -7,13 +7,13 @@ import framebuf
 import machine
 import utime
 
-sensor_temp = machine.ADC(26)
+sensor_temp = machine.ADC(28)
 conversion_factor = 3.3 / (65535)
 
 WIDTH  = 128                                            # oled display width
 HEIGHT = 64                                             # oled display height
 
-i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=400000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
+i2c = I2C(0, sda=Pin(16), scl=Pin(17), freq=200000)       # Init I2C using pins GP8 & GP9 (default I2C0 pins)
 print("I2C Address      : "+hex(i2c.scan()[0]).upper()) # Display device address
 print("I2C Configuration: "+str(i2c))                   # Display I2C config
 
@@ -42,3 +42,4 @@ while True:
 
     # Finally update the oled display so the image & text is displayed
     oled.show()
+
